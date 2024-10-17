d17c0c751c45a25140f394216fe518cf2a50d8d5
ylvaeSMHI <145473927+ylvaeSMHI@users.noreply.github.com>
2024-10-17 13:59:25 +0200

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -31,19 +31,18 @@ processed_data = 0
 for year, year_log in linear_regression_log.items():
         path = data_path + str(year) + path_co2
         for cruise_key, my_expedition in year_log.items():
-            for cruise_period in my_expedition["regression_periods"]:
+            for cruise_period in my_expedition["cruise_period"]:
                 print(cruise_period)
                 logger.info(cruise_period)
-            try:
-                cruise = Cruise(path, cruise_period, data_save_path="C:/LenaV/python3/w_pCO2/processed_data")
-                measurement_data, ZeroCycle = cruise.get_data_package()
-                #data = ProcessData(path=path, regression_period=regression_period)
-            except AssertionError as e:
-                print(e)
-                continue
-            process = ProcessData(data_save_path="C:/LenaV/python3/w_pCO2/processed_data", measurements = measurement_data, zerocycles = ZeroCycle, regression_period = cruise_period)
-            process.process_data()
-            if process.valid:
-                processed_data += 1
-            else:
-                print(f"invalid data for {year}, {cruise_key}, {cruise_period}")
+                try:
+                    cruise = Cruise(path, cruise_period, data_save_path="C:/LenaV/python3/w_pCO2/processed_data")
+                    measurement_data, ZeroCycle = cruise.get_data_package()
+                except AssertionError as e:
+                    print(e)
+                    continue
+                process = ProcessData(data_save_path="C:/LenaV/python3/w_pCO2/processed_data", measurements = measurement_data, zerocycles = ZeroCycle, regression_period = cruise_period)
+                process.process_data()
+                if process.valid:
+                    processed_data += 1
+                else:
+                    print(f"invalid data for {year}, {cruise_key}, {cruise_period}")
